05ab32d7917e7080f94147a430dda258c4573b5c
keshu kumar <keshukumar1909@gmail.com>
2025-06-14 20:25:00 +0530

    Adding the Support Page

--- src/landing_page/support/CreateTicket.js
+++ src/landing_page/support/CreateTicket.js
@@ -1,9 +1,78 @@
 import React from 'react'
 function CreateTicket() {
-    return (  
-         <h1>
-            CreateTicket
-        </h1>
+    return (
+        <div className='container'>
+            <h1 className='text-muted fs-2 p-5'>To create a ticket, select a relevant topic</h1>
+
+            <div className='row text-primary p-5 pt-0' style={{lineHeight:"1.9"}}>
+                <div className='col-4'>
+                    <h6 style={{color:"black"}}>➕ Account Opening</h6>
+                    <ul className='list-unstyled'>
+                        <li>Resident individual</li>
+                        <li>Minor</li>
+                        <li>Non Resident Indian (NRI)</li>
+                        <li>Company, Partnership, HUF and LLP</li>
+                        <li>Glossary</li>
+                    </ul>
+                </div>
+                <div className='col-4'>
+                    <h6 style={{color:"black"}}>👤 Your Zerodha Account</h6>
+                    <ul className='list-unstyled'>
+                        <li>Your Profile</li>
+                        <li>Account modification</li>
+                        <li>Client Master Report (CMR) and DP</li>
+                        <li>Nomination</li>
+                        <li>Transfer and conversion of securities</li>
+                    </ul>
+                </div>
+                <div className='col-4'>
+                    <h6 style={{color:"black"}}>📊 Kite</h6>
+                    <ul className='list-unstyled'>
+                        <li>IPO</li>
+                        <li>Trading FAQs</li>
+                        <li>Margin Trading Facility (MTF) and Margins</li>
+                        <li>Charts and orders</li>
+                        <li>Alerts and Nudges</li>
+                        <li>General</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className='row text-primary p-5' style={{lineHeight:"1.9"}}>
+                <div className='col-4'>
+                    <h6 style={{color:"black"}}>💳 Funds</h6>
+                    <ul className='list-unstyled'>
+                        <li>Add money</li>
+                        <li>Withdraw money</li>
+                        <li>Add bank accounts</li>
+                        <li>eMandates</li>
+                    </ul>
+                </div>
+                <div className='col-4'>
+                    <h6 style={{color:"black"}}>📁 Console</h6>
+                    <ul className='list-unstyled'>
+                        <li>Portfolio</li>
+                        <li>Corporate actions</li>
+                        <li>Funds statement</li>
+                        <li>Reports</li>
+                        <li>Profile</li>
+                        <li>Segments</li>
+                    </ul>
+                </div>
+                <div className='col-4'>
+                    <h6 style={{color:"black"}}>🪙 Coin</h6>
+                    <ul className='list-unstyled'>
+                        <li>Mutual funds</li>
+                        <li>National Pension Scheme (NPS)</li>
+                        <li>Features on Coin</li>
+                        <li>Payments and Orders</li>
+                        <li>General</li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+
     );
 }
 
--- src/landing_page/support/Hero.js
+++ src/landing_page/support/Hero.js
@@ -1,10 +1,76 @@
-import React from 'react'
+import React from 'react';
+
 function Hero() {
-    return ( 
-         <h1>
-            Hero
-        </h1>
-     );
+    return (
+        <div className='bg-primary p-5'>
+            <div className='container bg-primary text-white mt-5'>
+                <div className='row'>
+
+                    {/* Left column */}
+                    <div className='col-6'>
+                        <h5 className='mb-4'>Support Portal</h5>
+                        <h3 className='mb-4'>
+                            Search for an answer or browse help topics to create a ticket
+                        </h3>
+
+                        <input
+                            className='p-3 mb-4'
+                            style={{ width: '80%', borderRadius: '5px', border: 'none' }}
+                            placeholder='Eg: how do I activate F&O, why is my order getting rejected'
+                        />
+
+                        <div className='row'>
+                            <div className='col-4'>
+                                <a href='#' className='text-white text-decoration-underline'>
+                                    Track account opening
+                                </a>
+                            </div>
+                            <div className='col-4'>
+                                <a href='#' className='text-white text-decoration-underline'>
+                                    Track system activation
+                                </a>
+                            </div>
+                            <div className='col-4'>
+                                <a href='#' className='text-white text-decoration-underline'>
+                                    Intraday
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Right column */}
+                    <div className='col-6 d-flex flex-column justify-content-start align-items-end text-white'>
+
+                        {/* Track tickets link */}
+                        <div className='mb-5'>
+                            <a href='#' className='text-white text-decoration-underline'>
+                                Track tickets
+                            </a>
+                        </div>
+
+                        {/* Featured section */}
+                        <div className='w-75 text-start'>
+                            <h5 className='mb-4 fw-bold'>Featured</h5>
+                            <ol className='text-white'>
+                                <li className='mb-2'>
+                                    <a href='#' className='text-white text-decoration-underline'>
+                                        MCX Crude option contract expiry – June 2025
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href='#' className='text-white text-decoration-underline'>
+                                        Latest Intraday leverages and Square-off timings
+                                    </a>
+                                </li>
+                            </ol>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Hero;
