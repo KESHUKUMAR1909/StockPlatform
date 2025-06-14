5998db21032974bd5c6d5c2b469b0b8dea0b1879
keshu kumar <keshukumar1909@gmail.com>
2025-06-14 15:20:55 +0530

    Making Team Section

--- src/landing_page/about/Team.js
+++ src/landing_page/about/Team.js
@@ -1,9 +1,42 @@
 import React from 'react'
 function Team() {
     return ( 
-         <h1>
-           
-        </h1>
+         <div className='row p-5 mt-5 border-top'>
+        <div className='row '> <h1 className='text-center mx-5 text-muted mt-5 '  style={{ marginLeft: '500px' }}>Profile</h1> </div>
+        <div className='row'>
+          <div className='col-6 p-5 text-muted' style={{ lineHeight: '1.3', fontSize: '18px' }} >
+            <div className='row'>
+              <img src='media/images/nithinKamath.jpg' className='rounded-circle mx-auto' style={{ width: "50%" }} alt='profileImage'></img>
+            </div>
+            <div className='row text-center text-muted'>
+              <h1> Nithin Kamath</h1>
+            </div>
+            <div className='row text-center text-muted'>
+              <p>Founder , CEO</p>
+            </div>
+
+          </div>
+          <div className='col-6 p-5 text-muted' style={{ fontSize: '18px' }}>
+            <p>
+              Nithin bootstrapped and founded Zerodha in 2010 to overcome the hurdles he faced during his decade long stint as a trader. Today, Zerodha has changed the landscape of the Indian broking industry.
+              <br />
+              <br />
+              He is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC).
+              <br />
+              <br />
+              Playing basketball is his zen.
+              <br />
+              <br />
+              Connect on Homepage / TradingQnA / Twitter
+            </p>
+          </div>
+        </div>
+
+
+
+      </div>
+
+
      );
 }
 
