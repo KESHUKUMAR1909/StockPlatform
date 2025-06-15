c9e08bd4376e15f8d201910950d355f2e8696701
keshu kumar <keshukumar1909@gmail.com>
2025-06-15 12:03:08 +0530

    Connection with db setup

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const PORT = process.env.PORT||3002;
+const uri = process.env.MONGO_URL;
+
+
+
+
 const app = express();
-app.listen(3000 , ()=>{
+
+
+// Connect with database
+const connectDB = async(uri)=>{
+    await mongoose.connect(uri);
+}
+
+
+app.listen(PORT , async()=>{
     console.log("Successfully created the server");
-})
+    console.log("Try Connnecting to DB");
+    await connectDB(uri);
+    console.log("Connected with db Successfully");
+});
